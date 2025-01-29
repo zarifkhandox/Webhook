00b687c00ad5fa0c1996b300b4dec9041e870307
zarifkhandox <149472330+zarifkhandox@users.noreply.github.com>
2025-01-29 16:05:57 +0600

    api.js

--- api.js
+++ api.js
@@ -16,7 +16,10 @@ app.use(cors({
 
 
 app.options('*', (req, res) => {
-    res.sendStatus(200); 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.sendStatus(200);
 });
 app.use(express.json());
 
