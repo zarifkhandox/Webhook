6fac155a8cd07d7285336fdaf707c75074ab8c93
zarifkhandox <149472330+zarifkhandox@users.noreply.github.com>
2025-01-29 20:17:44 +0600

    api.js

--- api.js
+++ api.js
@@ -65,7 +65,7 @@ app.all('/:apiUrl', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.API_PORT || process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`API server is running on http://localhost:${PORT}`);
 });
