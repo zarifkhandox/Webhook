29b51968d204fc8973ee951300f98a10145efa01
zarifkhandox <149472330+zarifkhandox@users.noreply.github.com>
2025-01-29 15:33:06 +0600

    server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.post('/encrypt', (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
+const PORT = process.env.SERVER_PORT || 3000;
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
