041cbf6fdc4fd489faf2e52634ced8c177303a01
zarifkhandox <149472330+zarifkhandox@users.noreply.github.com>
2025-01-29 15:37:30 +0600

    api.js

--- api.js
+++ api.js
@@ -58,3 +58,14 @@ const PORT = process.env.API_PORT || 3001;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`API server is running on http://localhost:${PORT}`);
 });
+process.on('SIGTERM', () => {
+    console.log("API Process is shutting down gracefully...");
+    db.close((err) => {
+        if (err) {
+            console.error('Error closing the database connection:', err);
+        } else {
+            console.log('Database connection closed');
+        }
+        process.exit(0);
+    });
+});
