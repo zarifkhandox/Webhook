c1fef15bf677a4f23784749dd0d09e04b3f18d48
zarifkhandox <149472330+zarifkhandox@users.noreply.github.com>
2025-01-29 19:32:54 +0600

    api.js

--- api.js
+++ api.js
@@ -62,8 +62,8 @@ axios({
 });
 
 
-const PORT = process.env.API_PORT || 3001;
-app.listen(PORT, '0.0.0.0', () => {
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
     console.log(`API server is running on http://localhost:${PORT}`);
 });
 process.on('SIGTERM', () => {
